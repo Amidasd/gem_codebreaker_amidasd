8fb84a43ed4dc2f575c1af08c665cd4eafe09b63
Amidasd <amidasd@gmail.com>
2019-08-12 22:50:38 +0300

    0.1.3

--- lib/gem_codebreaker_amidasd.rb
+++ lib/gem_codebreaker_amidasd.rb
@@ -1,6 +1,10 @@
 require 'gem_codebreaker_amidasd/version'
 require 'gem_codebreaker_amidasd/gem_codebreaker'
+require 'gem_codebreaker_amidasd/db_utility'
+require 'gem_codebreaker_amidasd/user'
+require 'gem_codebreaker_amidasd/version'
 
+# Dir[Dir.pwd + './gem_codebreaker_amidasd/**/*.rb'].sort.reverse_each { |f| require f }
 # module GemCodebreakerAmidasd
 # #   class Error < StandardError; end
 # #
